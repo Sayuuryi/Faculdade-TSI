5266aaaa4db7f8877fe2b1c52b6ea0c89d0be644
Joao Victor R <sayurirhea@gmail.com>
2025-09-04 20:32:23 -0300

    Adcionando exercicio 3

---
+++ Programação Orientada a Objetos II/Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[] args) {    
+    int ftentrada = 200 * ; // Entrada de pés
+    double fttocm = 30.48; // Conversao de 1 pé para cm
+    int cm = 100; 
+    double meter = fttocm / cm;
+ }
+}
